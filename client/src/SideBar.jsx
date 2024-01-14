a8777a3efd41c420a29e8478be1360fb9befc689
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-13 17:00:04 -0800

    Add Sidebar componet
    This componet will add addition functionality and features. Such as the ability to add templates and upload

---
+++ client/src/SideBar.jsx
@@ -0,0 +1,14 @@
+function SideBar() {
+  return (
+    <div>
+      <aside>
+        <section className="widget">Templates</section>
+        <section className="widget">Elements</section>
+        <section className="widget">Uploads</section>
+        <section className="widget">Projects</section>
+      </aside>
+    </div>
+  );
+}
+
+export default SideBar;

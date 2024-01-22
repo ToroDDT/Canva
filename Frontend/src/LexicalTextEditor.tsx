c749fcb006eea6ab9156b554f4859264404a8a94
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-22 15:50:51 -0800

    Add plugins and created template for simple text editior

---
+++ Frontend/src/LexicalTextEditor.tsx
@@ -0,0 +1,20 @@
+import { $getRoot, $getSelection } from "lexical";
+import { useEffect } from "react";
+
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}

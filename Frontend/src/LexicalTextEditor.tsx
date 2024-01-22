bd5b0f3ab445e685adf7cf281b78f32a00580748
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-22 15:55:50 -0800

    Create a simple text editor template

--- Frontend/src/LexicalTextEditor.tsx
+++ Frontend/src/LexicalTextEditor.tsx
@@ -8,7 +8,9 @@ import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-
+const theme = {
+  color: "blue",
+};
 function MyCustomAutoFocusPlugin() {
   const [editor] = useLexicalComposerContext();
 
@@ -17,4 +19,28 @@ function MyCustomAutoFocusPlugin() {
   }, [editor]);
 
   return null;
+}
+
+function onError(error) {
+  console.error(error);
+}
+
+export default function Editor() {
+  const initialConfig = {
+    namespace: "MyEditor",
+    theme,
+    onError,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+      <MyCustomAutoFocusPlugin />
+    </LexicalComposer>
+  );
 }

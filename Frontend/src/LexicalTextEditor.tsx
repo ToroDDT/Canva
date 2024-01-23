b9ef1a8f5f9c7e7d7db358d81618271fd7f0a3c0
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-01-22 18:34:40 -0800

    Create plugin to set state when text is inputed

--- Frontend/src/LexicalTextEditor.tsx
+++ Frontend/src/LexicalTextEditor.tsx
@@ -1,16 +1,17 @@
-import { $getRoot, $getSelection } from "lexical";
+import { $getRoot, $getSelection, ElementNode } from "lexical";
 import { useEffect, useState } from "react";
-
+import { LexicalNode } from "lexical";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
-import { OnChangePlugin } from "@lexical/react/LexicalOnChangePlugin";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
 const theme = {
   color: "blue",
 };
+
 function MyCustomAutoFocusPlugin() {
   const [editor] = useLexicalComposerContext();
 
@@ -25,9 +26,24 @@ function onError(error) {
   console.error(error);
 }
 
+function OnChangePlugin({ onChange }) {
+  const [editor] = useLexicalComposerContext();
+  useEffect(() => {
+    return editor.registerUpdateListener(({ editorState }) => {
+      onChange(editorState);
+    });
+  }, [editor, onChange]);
+}
+
+function ElementNode() {
+  return <h1></h1>;
+}
+
 export default function Editor() {
   const [editorState, setEditorState] = useState();
+
   function onChange(editorState) {
+    const editorStateJSON = editorState.toJSON();
     setEditorState(editorState);
   }
   const initialConfig = {
@@ -44,19 +60,8 @@ export default function Editor() {
         ErrorBoundary={LexicalErrorBoundary}
       />
       <HistoryPlugin />
+      <OnChangePlugin onChange={onChange} />
       <MyCustomAutoFocusPlugin />
     </LexicalComposer>
   );
-}
-
-function OnChangePlugin({ onChange }) {
-  const [editor] = useLexicalComposerContext();
-  useEffect(() => {
-    return editor.registerUpdateListener(
-      ({ editorState }) => {
-        onChange(editorState);
-      },
-      [editor, onChange]
-    );
-  });
 }
